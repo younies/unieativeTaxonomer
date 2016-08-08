5c1177e97821315ba56e02dc6e41889cd4f4b025
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-08-08 11:53:00 -0500

    adding the starting files

---
+++ unieativeTaxonomer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  unieativeTaxonomer
+//
+//  Created by Younies Mahmoud on 8/8/16.
+//  Copyright © 2016 Younies Mahmoud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
