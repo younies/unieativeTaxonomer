d8e4c63ce7ff226bbb20866c1595eb5f07e71016
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-08-08 15:39:53 -0500

    no message

--- unieativeTaxonomer/main.cpp
+++ unieativeTaxonomer/main.cpp
@@ -10,7 +10,7 @@
 #include "YRJObject.hpp"
 
 
-string pathe = "/Users/youniesmahmoud/Desktop/903893.yrj";
+string pathe = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase_new_31/all.yrj";
 
 int main(int argc, const char * argv[]) {
     // insert code here...
