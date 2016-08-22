f5c050cab4b89aded1e63780ab32ad22bfc132be
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-08-21 21:19:09 -0500

    adding the random file

--- unieativeTaxonomer/main.cpp
+++ unieativeTaxonomer/main.cpp
@@ -14,13 +14,48 @@
 #include "Tree.hpp"
 
 string pattern = "##-#--###---#-#-#-#-#--#-##--##";
-//string pathe  = "/Users/youniesmahmoud/Desktop/903893.yrj";// = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase_new_31/all.yrj";
+string pathe   = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase_new_31/all.yrj";
+string million = "/export1/project/hondius/newKrakenResearch/generate_unique_random/MillionRandomIndices.txt";
+string tenMillion = "/export1/project/hondius/newKrakenResearch/generate_unique_random/TenMillionRandom.txt";
+
+
+string writeIN ="/export1/project/hondius/newKrakenResearch/generate_unique_random/";
+
+LONGS getLong( string s)
+{
+    stringstream ss(s);
+    LONGS ret;
+    ss >> ret;
+    return ret;
+}
+
+
+
+void writeFile(vector<LONG> & kmers , LONG kmerSize , string path)
+{
+    ofstream output_file(path);
+    
+    
+    LONG kmerNumbers = kmers.size();
+    
+    output_file.write( (char *) &kmerSize ,  sizeof(LONG));
+    output_file.write( (char *) &kmerNumbers ,  sizeof(LONG));
+    
+    for (int i = 0 ; i < kmerNumbers ; ++i)
+    {
+        output_file.write( (char *) &kmers[i] ,  sizeof(LONG));
+    }
+    
+    output_file.flush();
+    output_file.close();
+
+}
 
 int main(int argc, const char * argv[])
 {
     // insert code here...
     
-    
+   /*
    Tree * tree = new Tree(path_to_the_tree);
     cout << tree->getNumberOfLeaves(0) << endl;
 
@@ -28,11 +63,13 @@ int main(int argc, const char * argv[])
     
     vector<YRJObject*> yrjObj = tree->getYRJobjects(path_to_the_yrj_databases);
     
-    /*
+  
     vector<YRJObject*>::const_iterator first = yrjObj.begin() + 0;
     vector<YRJObject*>::const_iterator last = yrjObj.begin() + 10;
     vector<YRJObject*> newVec(first, last);
-    */
+   
+    
+    
     CoreTaxonomer * core = new CoreTaxonomer(yrjObj , pattern);
     
     core->writeTheCoreData(path_to_unieative_all_hashed);
@@ -40,6 +77,51 @@ int main(int argc, const char * argv[])
     HashedNode p =  core->getTheHashedKmer(1);
     
     cout <<sizeof(p) << endl;
+    
+    */
+    
+    YRJObject * getRandom = new YRJObject(pathe , 0);
+    
+    
+    ifstream ifsMil(million);
+    ifstream ifsTenMil(tenMillion);
+    
+    vector<LONG> tenMillionsInd;
+    vector<LONG> millionsInd;
+    
+    string temp;
+    while (getline(ifsMil, temp))
+    {
+        million.push_back( getLong( temp) );
+    }
+    
+    while (getline(ifsTenMil, temp))
+    {
+        tenMillionsInd.push_back( getLong( temp) );
+    }
+    
+    vector<LONG> tenMillions;
+    vector<LONG> millions;
+
+    
+    for (LONGS i = 0 ; i < tenMillionsInd.size() ; ++i)
+    {
+        tenMillions.push_back( getRandom->kmersVector[  tenMillionsInd[i] ] );
+    }
+    
+    for (LONGS i = 0 ; i < millionsInd.size() ; ++i)
+    {
+        millions.push_back( getRandom->kmersVector[  tenMillionsInd[i] ] );
+    }
+    
+    
+    sort(tenMillions.begin(), tenMillions.end());
+    sort(millions.begin(), millions.end());
+    
+    
+    
+    writeFile( millions , 31 , writeIN + "randomMillion.yrj");
+    writeFile( tenMillions , 31 , writeIN + "randomTenMillion.yrj");
     
     
     
