5b66920d518937516a3f224f54dc160044249fbf
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-10-17 14:11:54 -0500

    update testing ......

--- unieativeTaxonomer/testingYRJ.cpp
+++ unieativeTaxonomer/testingYRJ.cpp
@@ -11,7 +11,7 @@
 
 
 void Tester::testYRJvector(){
-    ifstream simBA5Stream("/export1/project/hondius/testingUnieative/dna.txt");
+    ifstream simBA5Stream(this->path_to_simBA5);
     ifstream headerSimBA5Stream(this->path_to_simBA5_headers);
     
     
@@ -29,8 +29,121 @@ void Tester::testYRJvector(){
     YRJObject yrj(DNA);
     
     
+    string pattern1 =  "##-#--###---#-#-#-#-#--#-##--##";
+
+    Hash * hash = new Hash(pattern1 , path_to_the_hashed_databases);
     
-    for(auto kmer : yrj.kmersVector)
-        cout << kmer << endl;
+    for(auto kmer : yrj.kmersVector){
+        auto differences = this->getNumerOfDifferences(hash, kmer);
+        
+        for(auto different : differences){
+            cout << different.first << "  " << different.second << endl;
+        }
+    }
+        
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vector< pair<short, short> >  Tester::getNumerOfDifferences(Hash * hash , LONG kmer){
+    vector< pair<short, short> >ret;
+    
+    //getting the streams
+    ifstream &index = *(hash->getIndexStream());
+    ifstream &data = *(hash->getDataStream());
+    
+    //getting the hashed Kmer
+    auto convertedKmer = hash->getHashedNode(kmer);
+    INT kmerIndex = hash->getINTfromPair(convertedKmer.rawKmer);
+    index.seekg(kmerIndex  * sizeof(HashIndex));
+    
+    HashIndex hashIndex;
+    index.read((char *) &hashIndex, sizeof(HashIndex));
+    
+    
+    
+    if(hashIndex.size == 0){
+        cout << "NOT found for kmer :" << kmer << endl;
+        return ret;
+    }
+    
+    
+    auto dataIndex = hashIndex.getIndex();
+    
+    data.seekg(dataIndex);
+    
+    vector<HashData> hashedData(hashIndex.size);
+    
+    for(int i = 0 , n = hashIndex.size ; i < n ; ++i)
+    {
+        data.read((char*) &hashedData[i], sizeof(HashData));
+    }
+    
+    
+    //add first kmer
+    ret.emplace_back(make_pair(hashedData[0].index, this->numOfDifferencesBetweenKmers(hashedData[0].hashedKmer, convertedKmer.hashedKmer)));
+    
+    for(auto hashedBlock : hashedData){
+        short diff = this->numOfDifferencesBetweenKmers(hashedBlock.hashedKmer, convertedKmer.hashedKmer);
+        
+        if(ret.back().first == hashedBlock.index)
+            ret.back().second = min(ret.back().second , diff);
+        else
+            ret.emplace_back(make_pair(hashedBlock.index, diff));
+    }
+    
+    
+    return ret;
+    
+    
+    
+    
+}
+
+
+
+
+
+
+short Tester::numOfDifferencesBetweenKmers(pair<short, short> hashedKmer1 , pair<short, short> hashedKmer2)
+{
+    
+    short differences = 0;
+    
+    
+    while (hashedKmer1.first != hashedKmer2.first)
+    {
+        if(hashedKmer1.first % 4 != hashedKmer2.first % 4)
+            ++differences;
+        
+        hashedKmer1.first /= 4;
+        hashedKmer2.first /= 4;
+    }
+    
+    while (hashedKmer1.second != hashedKmer2.second)
+    {
+        if(hashedKmer1.second % 4 != hashedKmer2.second % 4)
+            ++differences;
+        hashedKmer1.second /= 4;
+        hashedKmer2.second /= 4;
+    }
+    
+    return differences;
+}
+
+
--- unieativeTaxonomer/testingYRJ.hpp
+++ unieativeTaxonomer/testingYRJ.hpp
@@ -11,17 +11,42 @@
 
 #include "headers.h"
 #include "YRJObject.hpp"
+#include "BigTree.hpp"
+#include "Tree.hpp"
+#include "Hash.hpp"
+#include "helpers.hpp"
+#include "CoreTaxonomer.hpp"
 
 class Tester {
-    vector<YRJObject* > yrjVector;
-    
+
+    //constant we need in our program
     const string path_to_simBA5 = "/export1/project/hondius/testingUnieative/accuracy/simBA5_accuracy.fa";
     const string path_to_simBA5_headers = "/export1/project/hondius/testingUnieative/accuracy/mappingGI.txt";
+    const string path_to_the_hashed_databases = "/export1/project/hondius/unieative/databases/";
+
+    
+    vector<YRJObject* > yrjVector;
+    
+    BigTree * bigTree;
+    Tree * pruinedTree;
+    
+    vector<Hash *> hashes;
+    
+
+    
     
 public:
     
     
+    
+    
     void testYRJvector();
+    
+    vector< pair<short, short> > getNumerOfDifferences(Hash * hash , LONG kmer);
+
+    //return the difference between two hashed kmers
+    short numOfDifferencesBetweenKmers(pair<short, short> hashedKmer1 , pair<short, short> hashedKmer2);
+
 };
 
 
