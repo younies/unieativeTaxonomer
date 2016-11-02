26fdcc91b6ca02a60c93629d4ebd395135ce07da
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-11-02 12:57:38 -0500

    g

--- unieativeTaxonomer/testingYRJ2.cpp
+++ unieativeTaxonomer/testingYRJ2.cpp
@@ -82,7 +82,7 @@ void Tester::testingGenomeLevel(YRJObject * yrj   , int differences)
     
     //to get kraken Final Taxonomy
     
-    if(hitNumbers.size() < 20)
+    if(hitNumbers.size() < 5)
     {
         this->finalResult[this->notConsidered] ++;
         return;
