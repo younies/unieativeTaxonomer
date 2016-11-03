484b91297a558685c7133b79d0cb761971dcb79b
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-11-03 00:54:17 -0500

    no message

--- unieativeTaxonomer/testingYRJ.hpp
+++ unieativeTaxonomer/testingYRJ.hpp
@@ -42,7 +42,7 @@ class Tester {
     const string path_to_the_names_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/names.txt";
     const string path_to_the_nodes_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/nodes.txt";
 
-    const string result = "/export1/project/hondius/testingUnieative/matlabResults/kraken_Limit_10";
+    const string result = "/export1/project/hondius/testingUnieative/matlabResults/kraken_Limit_20_";
     vector<YRJObject* > yrjVector;
     
     BigTree * bigTree;
