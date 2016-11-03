a0a67921da0cf157ca75117950b880f17c1d1e33
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-11-03 01:05:36 -0500

    no message

--- unieativeTaxonomer/testingYRJ.cpp
+++ unieativeTaxonomer/testingYRJ.cpp
@@ -94,7 +94,7 @@ void Tester::testYRJvector(int deep){
         getline(hiseqNew, DNA);
         YRJObject yrj(DNA);
         yrj.uid = uid;
-        if(uid != 1321367)
+        if(uid != 1437877)
             continue;
         if (bigTree->getGenusUID(uid) == -1)
         {
