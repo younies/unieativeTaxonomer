ab44cc542676042f804f21e34fb403d206913139
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-12-13 23:39:24 -0600

    no message

--- unieativeTaxonomer/testingYRJ.hpp
+++ unieativeTaxonomer/testingYRJ.hpp
@@ -43,7 +43,7 @@ class Tester {
     const string path_to_the_names_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/names.txt";
     const string path_to_the_nodes_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/nodes.txt";
 
-    const string result = "/export1/project/hondius/testingUnieative/newTest/new_kraken_222/kmer_distributions_mi_";
+    const string result = "/export1/project/hondius/testingUnieative/newTest/new_kraken_222/test_genome_kraken_new_";
     
     const string path_to_Kraken_test = "/export1/project/hondius/testingUnieative/finalResults/ids_for_kraken_HiSeq.txt";
     
